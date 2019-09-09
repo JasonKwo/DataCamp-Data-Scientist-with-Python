fb74b0cb562fdba750e74f2c86898db8ec7088b5
Jason Kwo <51009700+JasonKwo@users.noreply.github.com>
2019-09-09 14:13:52 +1000

    Update Snippets-of-code-and-observations.py

--- 7-Cleaning-Data-in-Python/Snippets-of-code-and-observations.py
+++ 7-Cleaning-Data-in-Python/Snippets-of-code-and-observations.py
@@ -83,14 +83,26 @@ print(airquality_pivot_reset.index)
 
 #------------------------------------------------------------------------------------------------------------------------#
 
-
-
-
+airquality_pivot = airquality_dup.pivot_table(index=['Month', 'Day'], columns='measurement', values='reading', aggfunc=np.mean)
+airquality_pivot = airquality_pivot.reset_index()
 
 
 
 
 #------------------------------------------------------------------------------------------------------------------------#
 
+# Melt ebola: ebola_melt
+ebola_melt = pd.melt(ebola, id_vars=['Date', 'Day'], var_name='type_country', value_name='counts')
 
+# Create the 'str_split' column
+ebola_melt['str_split'] = ebola_melt.type_country.str.split('_')
+
+# Create the 'type' column
+ebola_melt['type'] = ebola_melt.str_split.str.get(0)
+
+# Create the 'country' column
+ebola_melt['country'] = ebola_melt.str_split.str.get(1)
+
+# Print the head of ebola_melt
+print(ebola_melt.head())
 
