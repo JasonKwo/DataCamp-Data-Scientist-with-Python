b206bcf362861100e54667d445cb6935ad64c9ca
Jason Kwo <51009700+JasonKwo@users.noreply.github.com>
2019-10-02 13:22:26 +1000

    Create Concat-and-append.py

---
+++ 10-Merging-DataFrames-with-Pandas/Concat-and-append.py
@@ -0,0 +1,29 @@
+'''
+You're now going to revisit the sales data you worked with earlier in the chapter.
+Three DataFrames jan, feb, and mar have been pre-loaded for you.
+Your task is to aggregate the sum of all sales over the 'Company' column into a single DataFrame.
+You'll do this by constructing a dictionary of these DataFrames and then concatenating them.
+'''
+
+
+
+# Make the list of tuples: month_list
+month_list = [('january',jan),('february',feb),('march',mar)]
+
+# Create an empty dictionary: month_dict
+month_dict = {}
+
+for month_name, month_data in month_list:
+
+    # Group month_data: month_dict[month_name]
+    month_dict[month_name] = month_data.groupby('Company').sum()
+
+# Concatenate data in month_dict: sales
+sales = pd.concat(month_dict)
+
+# Print sales
+print(sales)
+
+# Print all sales by Mediacore
+idx = pd.IndexSlice
+print(sales.loc[idx[:, 'Mediacore'], :])
