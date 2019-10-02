7f1abb92d44817a65adaea1c3ad24cff81a268c1
Jason Kwo <51009700+JasonKwo@users.noreply.github.com>
2019-10-02 13:51:41 +1000

    Update Concat-and-append.py

--- 10-Merging-DataFrames-with-Pandas/Concat-and-append.py
+++ 10-Merging-DataFrames-with-Pandas/Concat-and-append.py
@@ -27,3 +27,32 @@ print(sales)
 # Print all sales by Mediacore
 idx = pd.IndexSlice
 print(sales.loc[idx[:, 'Mediacore'], :])
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+'''
+In this exercise, you'll compare the historical 10-year GDP (Gross Domestic Product) growth in the US and in China.
+The data for the US starts in 1947 and is recorded quarterly; by contrast, the data for China starts in 1961 and is
+recorded annually.
+
+You'll need to use a combination of resampling and an inner join to align the index labels. You'll need an appropriate
+offset alias for resampling, and the method .resample() must be chained with some kind of aggregation method (.pct_change()
+and .last() in this case).
+
+pandas has been imported as pd, and the DataFrames china and us have been pre-loaded,
+'''
+
+
+# Resample and tidy china: china_annual
+# Chain resample (to get annual sample), last (to take last data point data), pct_change(10) (to compute
+# the percentage change with an offset of ten years), and dropna (to remove null values)
+china_annual = china.resample('A').last().pct_change(10).dropna()
+
+# Resample and tidy us: us_annual
+us_annual = us.resample('A').last().pct_change(10).dropna()
+
+# Concatenate china_annual and us_annual: gdp
+gdp = pd.concat([china_annual,us_annual],join='inner',axis=1)
+
+# Resample gdp and print
+print(gdp.resample('10A').last())
