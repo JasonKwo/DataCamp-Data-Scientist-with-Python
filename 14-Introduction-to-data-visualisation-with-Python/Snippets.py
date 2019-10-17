09617f95ab0be78a3d3a1ff0d4c3b0a5bb824c0c
Jason Kwo <51009700+JasonKwo@users.noreply.github.com>
2019-10-17 18:54:49 +1100

    Update Snippets.py

--- 14-Introduction-to-data-visualisation-with-Python/Snippets.py
+++ 14-Introduction-to-data-visualisation-with-Python/Snippets.py
@@ -1,3 +1,10 @@
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Set the style to 'ggplot'
+plt.style.use('ggplot')
+
+
 # Plot with legend as before
 plt.plot(year, computer_science, color='red', label='Computer Science') 
 plt.plot(year, physical_sciences, color='blue', label='Physical Sciences')
@@ -16,6 +23,85 @@ plt.annotate('Maximum', xy=(yr_max,cs_max), xytext=(yr_max+5,cs_max+5), arrowpro
 plt.xlabel('Year')
 plt.ylabel('Enrollment (%)')
 plt.title('Undergraduate enrollment of women')
+plt.show()
+
+
+#---------------------------------------------------------------------------------------------------------
+
+
+
+import numpy as np
+
+u = np.linspace(-2,2,3)
+v = np.linspace(-1,1,5)
+
+X,Y = np.meshgrid(u,v)
+
+Z = X**2/25 + Y**2/4
+
+
+# Create a filled contour plot with a color map of 'viridis'
+plt.subplot(2,2,1)
+plt.contourf(X,Y,Z,20, cmap='viridis')
+plt.colorbar()
+plt.title('Viridis')
+
+# Create a filled contour plot with a color map of 'gray'
+plt.subplot(2,2,2)
+plt.contourf(X,Y,Z,20, cmap='gray')
+plt.colorbar()
+plt.title('Gray')
+
+# Create a filled contour plot with a color map of 'autumn'
+plt.subplot(2,2,3)
+plt.contourf(X,Y,Z,20, cmap='autumn')
+plt.colorbar()
+plt.title('Autumn')
+
+# Create a filled contour plot with a color map of 'winter'
+plt.subplot(2,2,4)
+plt.contourf(X,Y,Z,20, cmap='winter')
+plt.colorbar()
+plt.title('Winter')
+
+# Improve the spacing between subplots and display them
+plt.tight_layout()
+plt.show()
+
+
+#---------------------------------------------------------------------------------------------------------
+
+# 2D histogram to show that cars with higher horsepower have lower fuel efficiency
+
+# Generate a 2-D histogram
+plt.hist2d(hp,mpg,bins=(20,20),range=((40,235),(8,48)))
+
+# Add a color bar to the histogram
+plt.colorbar()
+
+# Add labels, title, and display the plot
+plt.xlabel('Horse power [hp]')
+plt.ylabel('Miles per gallon [mpg]')
+plt.title('hist2d() plot')
+plt.show()
+
+
+
+#---------------------------------------------------------------------------------------------------------
+
+# 2D Hexagonal bin histogram
+
+# Generate a 2d histogram with hexagonal bins
+plt.hexbin(hp,mpg,gridsize=(15,12),extent=(40,235,8,48))
+
+           
+# Add a color bar to the histogram
+plt.colorbar()
+
+# Add labels, title, and display the plot
+plt.xlabel('Horse power [hp]')
+plt.ylabel('Miles per gallon [mpg]')
+plt.title('hexbin() plot')
 plt.show()
 
 
